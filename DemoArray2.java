e5c8610ad6174b1f7e56f8fa2733a87d803d6606
TurtleGITHUB <76065011+TurtleGITHUB@users.noreply.github.com>
2021-03-12 09:04:37 -0500

    Original Upload

---
+++ DemoArray2.java
@@ -0,0 +1,18 @@
+// Chapter 8 tutorial exercise
+
+public class DemoArray2
+{
+	public static void main(String[] args)
+	{
+		double[] salaries = {12.25, 13.55, 14.25, 16.85};
+
+
+		System.out.println("Salaries one by one are:");
+		for(int x = 0; x < salaries.length; ++x)
+		{
+			System.out.println(salaries[x]);
+				
+		}
+						
+	}
+}
